012a56b81565a4d6035afc4257ea2c45f1a81cfb
PownallBentley <bentleypownall@gmail.com>
2026-01-02 13:57:29 +0000

    Updated index.ts

---
+++ src/components/subjects/index.ts
@@ -0,0 +1,11 @@
+// src/components/subjects/index.ts
+
+export { default as ChildInfoBanner } from "./ChildInfoBanner";
+export { default as OverviewCards } from "./OverviewCards";
+export { default as SubjectCard } from "./SubjectCard";
+export { default as CoverageSummary } from "./CoverageSummary";
+export { default as ReassuranceCard } from "./ReassuranceCard";
+export { default as FocusAreasCard } from "./FocusAreasCard";
+export { default as TimelineView } from "./TimelineView";
+export { default as SuggestionsCard } from "./SuggestionsCard";
+export { default as QuickActions } from "./QuickActions";
