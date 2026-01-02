d7c1abe2e7fa291e12a1f6fe40eb6cc99ff11435
PownallBentley <bentleypownall@gmail.com>
2026-01-02 13:00:42 +0000

    Updated index.ts

---
+++ src/components/dashboard/index.ts
@@ -0,0 +1,8 @@
+// src/components/dashboard/index.ts
+
+export { default as ChildCard } from "./ChildCard";
+export { default as WeekSummary } from "./WeekSummary";
+export { default as DailyActivityChart } from "./DailyActivityChart";
+export { default as GentleReminders } from "./GentleReminders";
+export { default as ComingUpNext } from "./ComingUpNext";
+export { default as SubjectCoverage } from "./SubjectCoverage";
