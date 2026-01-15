ac0236fa58052f824a9192220445cbadb1324587
PownallBentley <bentleypownall@gmail.com>
2026-01-15 22:02:36 +0000

    Updated index.ts

--- src/components/parent/dashboard/index.ts
+++ src/components/parent/dashboard/index.ts
@@ -4,3 +4,12 @@
 export { HeroStatusBanner } from "./HeroStatusBanner";
 export { ChildHealthCard } from "./ChildHealthCard";
 export { ChildHealthCardGrid } from "./ChildHealthCardGrid";
+export { WeeklyFocusStrip } from "./WeeklyFocusStrip";
+export { ComingUpCard } from "./ComingUpCard";
+export { HelpfulNudgesCard } from "./HelpfulNudgesCard";
+export { ProgressMomentsCard } from "./ProgressMomentsCard";
+export { SupportTipCard } from "./SupportTipCard";
+export { QuickActionsSection } from "./QuickActionsSection";
+export { FamilyOverviewCard } from "./FamilyOverviewCard";
+export { WeeklyRhythmChart } from "./WeeklyRhythmChart";
+export { ResourcesSection } from "./ResourcesSection";
